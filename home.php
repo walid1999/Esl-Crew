821827eccb2968905860c81a2b4890ab38e5bb94
Walid <walid.benkhelfallah@ynov.com>
2020-09-28 10:05:14 +0200

    update

---
+++ home.php
@@ -0,0 +1,1 @@
+<?php include("inc/header.inc.php"); ?>
---
+++ inc/header.log.inc.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+  <title></title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">  
+
+  <!-- Custom styles for this template -->
+  <link href="css/resume.min.css" rel="stylesheet">
+  <link href="css/resume.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="index.php">
+      <span class="d-none d-lg-block">
+        <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt="">
+      </span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+       
+
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="annonces.php">Annonces</a>
+        </li>
+        
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="mesannonces.php">Mes annonces</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="ajoutannonces.php">Ajouter une annonce</a>
+        </li>
+       
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="profil.php">Profil</a>
+        </li>
+
+        
+       
+      </ul>
+    </div>
+  </nav>
+
+  <?php include("inc/data.inc.php") ?>
